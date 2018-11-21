841a5568b5610877c1964d0b24a4c4ac081cf4a8
teodortita <teotita98@gmail.com>
2018-11-21 19:56:17 +0200

    Change main container column offset

--- index.php
+++ index.php
@@ -29,7 +29,7 @@ include 'conn.php';
 <body>
 	<div class="container">
 		<div class="row">
-			<div class="col-md-6">
+			<div class="col-md-6 offset-md-3">
 				<div class="card my-5">
 					<div class="card-body">
 
